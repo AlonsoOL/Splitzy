6202f4fb8953eb82050335d58173742377251058
IvanMontesGuti <162334629+IvanMontesGuti@users.noreply.github.com>
2025-04-17 19:10:48 +0200

    primeras rutas creadas

--- Frontend/Splitzy-Front/src/app/App.jsx
+++ Frontend/Splitzy-Front/src/app/App.jsx
@@ -2,11 +2,20 @@ import { Routes, Route } from "react-router-dom";
 import './App.css'
 import Login from "./login/login.jsx";
 
+
+function Home(){
+  return (
+    <>
+      <div>Home Page</div>
+    </>
+  )
+}
+
 function App() {
   return (
       <div>
         <Routes>
-          <Route path="/" element={<div>Home</div>} />
+          <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
         </Routes>
       </div>
