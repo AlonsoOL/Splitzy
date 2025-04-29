a24b2a5c03a05fa936250928efbf8f1c5a5b3bbf
IvanMontesGuti <162334629+IvanMontesGuti@users.noreply.github.com>
2025-04-29 20:32:38 +0200

    login

--- Frontend/splitzy-front/src/app/App.tsx
+++ Frontend/splitzy-front/src/app/App.tsx
@@ -2,6 +2,7 @@ import './App.css'
 import { Route, Routes } from 'react-router-dom'
 import Register from '../pages/Register'
 import Header from '../components/Header'
+import Login from '@/pages/Login'
 
 function Home(){
   return(
@@ -17,6 +18,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/register' element={<Register/>}/>
+        <Route path='/login' element={<Login/>}/>
       </Routes>
     </>
   )
