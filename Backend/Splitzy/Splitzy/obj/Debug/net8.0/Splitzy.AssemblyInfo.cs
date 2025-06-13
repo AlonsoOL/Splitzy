bf1eb4ee641013ea90053a0f7b18a5ca7e2658ae
IvanMontesGuti <162334629+IvanMontesGuti@users.noreply.github.com>
2025-06-13 15:13:07 +0200

    header y footer responsive

--- Backend/Splitzy/Splitzy/obj/Debug/net8.0/Splitzy.AssemblyInfo.cs
+++ Backend/Splitzy/Splitzy/obj/Debug/net8.0/Splitzy.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Splitzy")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b27837f44d25873364b7ca2d731149d23a8bfcb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c86276a785f0b65eae8467c15904e0c6ededcaa8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Splitzy")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Splitzy")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
